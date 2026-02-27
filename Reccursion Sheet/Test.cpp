17864014349c5c0358a372bb8216ba58b66e2fc3
Mazen Elemam <657889mj@gmail.com>
2026-02-27 15:31:01 +0200

    Solved First Problem "Print Recursion"

--- Reccursion Sheet/Test.cpp
+++ Reccursion Sheet/Test.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 using namespace std;
+void print(int n) {
+	if (n <= 0) {
+		return;
+	}
+	cout << "I love Recursion" << endl;
+	print(n - 1);
+}
+
 int main() {
-	cout << "hello world";
+	int n;
+	cin >> n;
+	print(n);
 	return 0;
 }
